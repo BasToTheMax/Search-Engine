a1dfcfffdac9f0521e067117e56d791ab4a4e7dd
BasToTheMax <bas@arnold-linda.nl>
2022-05-19 20:31:59 +0200

    queue size

---
+++ quesize.js
@@ -0,0 +1,44 @@
+(async () => {
+    const config = require('./config.json');
+    const knex = require('knex')({
+        client: 'mysql',
+        connection: config.database,
+        useNullAsDefault: true,
+        migrations: {
+            tableName: 'migrations'
+        },
+        debug: false,
+        log: {
+            warn(message) {
+                console.log('db - warn - ', message)
+            },
+            error(message) {
+                console.log('db - error - ', message)
+            },
+            deprecate(message) {
+                console.log('db - deprecate - ', message)
+            },
+            debug(message) {
+                console.log('db - ', message.__knexQueryUid + ': '+ message.sql)
+            },
+        }
+    });
+
+    var qc = await knex('sites')
+        .where({
+            name: 'In crawl queue'
+        }).count();
+
+    var ac = await knex('sites').count();
+
+    // console.log(qc, qc[0]['count(*)'], qc[0]);
+
+    console.clear();
+
+    console.log(`Total sites in queue: ${qc[0]['count(*)']}`);
+    console.log(`Total sites: ${ac[0]['count(*)']}`);
+    console.log(`Total crawled sites: ${ac[0]['count(*)'] - qc[0]['count(*)']}`);
+
+    process.exit();
+
+})();
